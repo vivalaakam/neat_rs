e0cdc085541b1e7387ba20d880fac43265ccc871
vivalaakam <viva.la.akam@gmail.com>
2022-10-05 06:39:00 +0300

    v0.1.16

--- src/organism.rs
+++ src/organism.rs
@@ -1,14 +1,16 @@
-use new_york_utils::Matrix;
 use std::cmp::Ordering;
+use std::sync::Mutex;
+
+use new_york_utils::Matrix;
 
 use crate::network::Network;
 use crate::{Config, Genome, NeuronType};
 
-#[derive(Clone, Default)]
+#[derive(Default)]
 pub struct Organism {
     pub genome: Genome,
     pub network: Network,
-    fitness: f64,
+    fitness: Mutex<f64>,
     stagnation: usize,
     genotype: Vec<String>,
     id: Option<String>,
@@ -29,7 +31,7 @@ impl Organism {
         Organism {
             genome,
             network,
-            fitness: 0f64,
+            fitness: Mutex::new(0f64),
             stagnation: 0,
             genotype,
             id: None,
@@ -44,12 +46,13 @@ impl Organism {
         self.network.activate_matrix(params)
     }
 
-    pub fn set_fitness(&mut self, fitness: f64) {
-        self.fitness = fitness;
+    pub fn set_fitness(&self, fitness: f64) {
+        let mut data = self.fitness.lock().unwrap();
+        *data = fitness
     }
 
     pub fn get_fitness(&self) -> f64 {
-        self.fitness
+        *self.fitness.lock().unwrap()
     }
 
     pub fn get_genotype(&self) -> Vec<String> {
@@ -70,11 +73,6 @@ impl Organism {
 
     pub fn as_json(&self) -> String {
         self.genome.as_json()
-    }
-
-    pub fn from_json(&self, data: String) -> Self {
-        let genome = serde_json::from_str::<Genome>(data.as_str()).unwrap();
-        Organism::new(genome)
     }
 
     pub fn set_stagnation(&mut self, stagnation: usize) {
@@ -98,9 +96,32 @@ impl Organism {
     }
 }
 
+impl Clone for Organism {
+    fn clone(&self) -> Self {
+        Organism {
+            genome: self.genome.clone(),
+            network: self.network.clone(),
+            fitness: Mutex::new(self.get_fitness()),
+            stagnation: self.stagnation.clone(),
+            genotype: self.genotype.clone(),
+            id: self.id.clone(),
+        }
+    }
+}
+
+impl From<String> for Organism {
+    fn from(data: String) -> Self {
+        let genome = serde_json::from_str::<Genome>(data.as_str()).unwrap();
+        Organism::new(genome)
+    }
+}
+
 impl Ord for Organism {
     fn cmp(&self, other: &Self) -> Ordering {
-        other.fitness.partial_cmp(&self.fitness).unwrap()
+        other
+            .get_fitness()
+            .partial_cmp(&self.get_fitness())
+            .unwrap()
     }
 }
 
@@ -108,7 +129,7 @@ impl Eq for Organism {}
 
 impl PartialEq for Organism {
     fn eq(&self, other: &Self) -> bool {
-        self.fitness == other.fitness
+        self.get_fitness() == other.get_fitness()
     }
 }
 
